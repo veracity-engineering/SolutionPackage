10a25de4a983ed59ba212f2f1fed36c879389b3b
Steven Mark Alfosteverd <ADMSTEALF@dnv.onmicrosoft.com>
2020-06-10 12:26:39 +0800

    Add basic user authentication test project.

---
+++ DNVGL.AuthTest.Web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DNVGL.AuthTest.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [Route("auth")]
+        public async Task<IActionResult> Auth()
+        {
+            /*
+            var scope = new[] { "https://dnvglb2ctest.onmicrosoft.com/efb3e529-2f80-458b-aedf-7f4c8c794b45" };// AzureAdB2COptions.ApiScopes.Split(' ');
+            var signedInUserID = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            IConfidentialClientApplication cca =
+            ConfidentialClientApplicationBuilder.Create("6f0bb6fa-e604-43cd-9414-42def1ac7deb")
+                .WithRedirectUri("")
+                .WithClientSecret("g.i1k-B_63p-oi5U6oQSL5V0DVY2iGZXJ~")
+                .WithAuthority("https://login.microsoftonline.com/dnvglb2ctest.onmicrosoft.com")
+                .Build();
+            //new MSALStaticCache(signedInUserID, this.HttpContext).EnablePersistence(cca.UserTokenCache);
+
+            var accounts = await cca.GetAccountsAsync();
+            AuthenticationResult result = await cca.AcquireTokenSilent(scope, accounts.FirstOrDefault()).ExecuteAsync();
+
+            return Json(result);
+            */
+            return Json(new { message = "Hello world" });
+        }
+    }
+}
---
+++ DNVGL.AuthTest.Web/Startup.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using System.Linq;
+
+namespace DNVGL.AuthTest.Web
+{
+    public partial class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var authBuilder = services.AddAuthentication(options =>
+            {
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            });
+
+            authBuilder.AddOpenIdConnect(options =>
+            {
+                var configManager = new ConfigurationManager<OpenIdConnectConfiguration>(
+                    HardConfig.OpenIdConnectEndpoint,//oauthOptions.GetOpenIdConnectEndpoint(),
+                    new OpenIdConnectConfigurationRetriever()
+                );
+                var config = configManager.GetConfigurationAsync().Result;
+
+                options.ClientId = HardConfig.ClientId;
+                options.ClientSecret = HardConfig.ClientSecret;
+                options.Authority = HardConfig.Authority;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKeys = config.SigningKeys,//.Concat(config_v1.SigningKeys),
+                    ValidIssuers = new[] { HardConfig.Authority },
+                    ValidAudiences = new[] { HardConfig.Audience },
+                };
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
