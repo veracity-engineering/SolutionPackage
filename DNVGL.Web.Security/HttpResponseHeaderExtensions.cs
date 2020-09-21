505139792c8e67cedd18611e017d4aa3d5b81668
He Ke Henry Zhang <He.Ke.Henry.Zhang@dnvgl.com>
2020-09-21 10:10:19 +0800

    Create  package to add default response headers

---
+++ DNVGL.Web.Security/HttpResponseHeaderExtensions.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DNVGL.Web.Security
+{
+    /// <summary>
+    /// Extension methods to Response.Headers  
+    /// </summary>
+    public static class HttpResponseHeaderExtensions
+    {
+        /// <summary>
+        ///<para> Adds and configures the predefined headers for Http response headers.</para> 
+        ///<para>TO avoid overwrite your own customized response header settings, call this method  at last. If the predefined headers is not desired, setup you desired headers before calling this method</para> 
+        /// </summary>
+        /// <remarks>
+        /// <para>The prefined security headers includes:</para>
+        /// <list type="bullet">
+        /// <item>
+        /// <description>X-Xss-Protection = 1</description>
+        /// </item>
+        /// <item>
+        /// <description>X-Frame-Options = SAMEORIGIN</description>
+        /// </item>
+        /// <item>
+        /// <description>Referrer-Policy = no-referrer</description>
+        /// </item>
+        /// <item>
+        /// <description>X-Content-Type-Options = nosniff</description>
+        /// </item>
+        /// <item>
+        /// <description>X-Permitted-Cross-Domain-Policies = none</description>
+        /// </item>
+        /// <item>
+        /// <description>Expect-CT = enforce, max-age=7776000</description>
+        /// </item>
+        /// <item>
+        /// <description>X-Xss-Protection = 1</description>
+        /// </item>
+        /// </list>
+        /// </remarks>
+        /// <param name="headerDictionary">The Response.Headers</param>
+        public static void SetupDefaultHeaders(this IHeaderDictionary headerDictionary)
+        {
+            headerDictionary.AddKeyOnlyIfNotExists("X-Xss-Protection", "1");
+            headerDictionary.AddKeyOnlyIfNotExists("X-Frame-Options", "SAMEORIGIN");
+            headerDictionary.AddKeyOnlyIfNotExists("Referrer-Policy", "no-referrer");
+            headerDictionary.AddKeyOnlyIfNotExists("X-Content-Type-Options", "nosniff");
+            headerDictionary.AddKeyOnlyIfNotExists("X-Permitted-Cross-Domain-Policies", "none");
+            headerDictionary.AddKeyOnlyIfNotExists("Expect-CT", "enforce, max-age=7776000");
+        }
+
+        private static void AddKeyOnlyIfNotExists(this IHeaderDictionary headerDictionary, string key, string value)
+        {
+            if (!headerDictionary.ContainsKey(key))
+            {
+                headerDictionary.Add(key, value);
+            }
+        }
+
+        internal static void AddContentSecurityPolicy(this IHeaderDictionary headerDictionary)
+        {
+            if (headerDictionary.ContainsKey("Content-Security-Policy") || headerDictionary.ContainsKey("Content-Security-Policy-Report-Only"))
+                return;
+
+            var csp = string.Join("; ", "default-src 'self'", "object-src 'self'", 
+                string.Join(" ", "connect-src 'self'", "https://dc.services.visualstudio.com"), 
+                string.Join(" ", "script-src 'self'", "https://www.recaptcha.net", "https://www.gstatic.com", "https://www.gstatic.cn"),
+                string.Join(" ", "font-src 'self' data:", "https://onedesign.azureedge.net"),
+                string.Join(" ", "media-src 'self'"),
+                string.Join(" ", "worker-src 'self' blob:"),
+                string.Join(" ", "img-src 'self' data:", "https://onedesign.azureedge.net"),
+                string.Join(" ", "frame-src 'self'", "https://www.google.com", "https://www.recaptcha.net/"),
+                string.Join(" ", "style-src 'self'","https://onedesign.azureedge.net")
+                );
+
+            headerDictionary.Add("Content-Security-Policy", new[] { csp });
+        }
+    }
+}
---
+++ DNVGL.Web.Security/ResponseHeadersMiddlewareExtensions.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace DNVGL.Web.Security
+{
+    /// <summary>
+    /// Default Response Headers extensions to <see cref="IApplicationBuilder"/>.
+    /// </summary>
+    public static class ResponseHeadersMiddlewareExtensions
+    {
+        /// <summary>
+        ///<para> Adds and configures the predefined headers for Http response headers. Content-Security-Policy header is not added to request which url contains 'swagger'</para> 
+        ///<para>TO avoid overwrite your own customized response header settings, call this method  at last. If the predefined headers is not desired, setup you desired headers before calling this method</para> 
+        /// </summary>
+        /// <remarks>
+        /// <para>The prefined security headers includes:</para>
+        /// <list type="bullet">
+        /// <item>
+        /// <description>X-Xss-Protection = 1</description>
+        /// </item>
+        /// <item>
+        /// <description>X-Frame-Options = SAMEORIGIN</description>
+        /// </item>
+        /// <item>
+        /// <description>Referrer-Policy = no-referrer</description>
+        /// </item>
+        /// <item>
+        /// <description>X-Content-Type-Options = nosniff</description>
+        /// </item>
+        /// <item>
+        /// <description>X-Permitted-Cross-Domain-Policies = none</description>
+        /// </item>
+        /// <item>
+        /// <description>Expect-CT = enforce, max-age=7776000</description>
+        /// </item>
+        /// <item>
+        /// <description>X-Xss-Protection = 1</description>
+        /// </item>
+        /// <item>
+        /// <description>Content-Security-Policy = default-src 'self'; object-src 'self'; connect-src 'self' https://dc.services.visualstudio.com; script-src 'self' https://www.recaptcha.net https://www.gstatic.com https://www.gstatic.cn; font-src 'self' data: https://onedesign.azureedge.net; media-src 'self'; img-src 'self' data: https://onedesign.azureedge.net; frame-src 'self' https://www.google.com https://www.recaptcha.net/;style-src 'self' https://onedesign.azureedge.net;worker-src 'self' blob:</description>
+        /// </item>
+        /// </list>
+        /// </remarks>
+        /// <param name="makeHeaders">make your own response headers, It will overwrite the default headers.</param>
+        /// <returns>The <see cref="IApplicationBuilder"/>.</returns>
+        /// <example>
+        /// This sample shows how to call the <see cref="UseDefaultHeaders"/> method in the Configure method of Startup class.
+        /// <code>
+        /// app.UseDefaultHeaders(responseHeaders.Add("X-Xss-Protection", "0"));
+        /// </code>
+        /// </example>
+        public static IApplicationBuilder UseDefaultHeaders(this IApplicationBuilder builder, Action<IHeaderDictionary> makeHeaders = null)
+        {
+            return builder.Use(async (context, next) =>
+            {
+                makeHeaders?.Invoke(context.Response.Headers);
+                context.Response.Headers.SetupDefaultHeaders();
+                if (!context.Request.Path.ToString().ToLowerInvariant().Contains("/swagger/"))
+                {
+                    context.Response.Headers.AddContentSecurityPolicy();
+                }
+                await next();
+            });
+        }
+    }
+}
