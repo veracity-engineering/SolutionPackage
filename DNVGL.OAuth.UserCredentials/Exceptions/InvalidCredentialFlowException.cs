ddd901ad37d05d7ecafd88177dce82e57c1495b4
Steven Mark Alfosteverd <ADMSTEALF@dnv.onmicrosoft.com>
2021-03-26 17:44:09 +0800

    Decorate custom exceptions with Serializable attribute.

--- DNVGL.OAuth.UserCredentials/Exceptions/InvalidCredentialFlowException.cs
+++ DNVGL.OAuth.UserCredentials/Exceptions/InvalidCredentialFlowException.cs
@@ -2,6 +2,7 @@
 
 namespace DNVGL.OAuth.Api.HttpClient.Exceptions
 {
+	[Serializable]
 	public class InvalidCredentialFlowException : Exception
 	{
 		public InvalidCredentialFlowException(OAuthCredentialFlow credentialFlow) : base($"Invalid credential flow '{credentialFlow}'.") { }
--- DNVGL.OAuth.UserCredentials/Exceptions/MissingClientConfigurationNameException.cs
+++ DNVGL.OAuth.UserCredentials/Exceptions/MissingClientConfigurationNameException.cs
@@ -2,6 +2,7 @@
 
 namespace DNVGL.OAuth.Api.HttpClient.Exceptions
 {
+	[Serializable]
 	public class MissingClientConfigurationNameException: Exception
 	{
 		public MissingClientConfigurationNameException(string name) : base($"No instance of {nameof(OAuthHttpClientFactoryOptions)} could be retrieved where Name = '{name}'.") { }
