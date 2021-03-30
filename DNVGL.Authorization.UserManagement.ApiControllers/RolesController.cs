2a5c65d28ced86560594b9a0639e671f37c50ce9
He Ke Henry Zhang <He.Ke.Henry.Zhang@dnvgl.com>
2021-03-30 13:14:19 +0800

    split corss company api and my company api

--- DNVGL.Authorization.UserManagement.ApiControllers/RolesController.cs
+++ DNVGL.Authorization.UserManagement.ApiControllers/RolesController.cs
@@ -16,23 +16,110 @@ namespace DNVGL.Authorization.UserManagement.ApiControllers
 {
     [Authorize]
     [ApiController]
-    [Route("api/roles")]
+    [Route("api/mycompany/roles")]
     public class RolesController : ControllerBase
     {
         private readonly IRole _roleRepository;
+        private readonly IUser _userRepository;
         private readonly ICompany _companyRepository;
         private readonly IPermissionRepository _permissionRepository;
+        private readonly PermissionOptions _premissionOptions;
 
-        public RolesController(IRole roleRepository, ICompany companyRepository, IPermissionRepository permissionRepository)
+        public RolesController(IUser userRepository, IRole roleRepository, ICompany companyRepository, IPermissionRepository permissionRepository, PermissionOptions premissionOptions)
         {
+            _userRepository = userRepository;
             _roleRepository = roleRepository;
             _companyRepository = companyRepository;
             _permissionRepository = permissionRepository;
+            _premissionOptions = premissionOptions;
         }
 
 
         [HttpGet]
         [Route("")]
+        [PermissionAuthorize(Premissions.ViewRole)]
+        public async Task<IEnumerable<RoleViewDto>> GetCompanyRoles()
+        {
+            var user = await GetCurrentUser();
+            return await GetRolesByCompanyId(user.CompanyId);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [PermissionAuthorize(Premissions.ViewRole)]
+        public async Task<Role> GetRole([FromRoute] string id)
+        {
+            var roles = await GetCompanyRoles();
+
+            if (roles.Any(t => t.Id == id))
+            {
+                return await FetchRole(id);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [HttpPost]
+        [Route("")]
+        [PermissionAuthorize(Premissions.ManageRole)]
+        public async Task<string> CreateRole([FromBody] RoleEditModel model)
+        {
+            var user = await GetCurrentUser();
+            var permissionKeys = await PrunePermissions(user.CompanyId, model.PermissionKeys);
+
+            var role = new Role
+            {
+                Description = model.Description,
+                Name = model.Name,
+                Active = model.Active,
+                CompanyId = user.CompanyId,
+                Permissions = string.Join(';', permissionKeys)
+            };
+            role = await _roleRepository.Create(role);
+            return role.Id;
+        }
+
+
+        [HttpPut]
+        [Route("{id}")]
+        [PermissionAuthorize(Premissions.ManageRole)]
+        public async Task UpdateRole([FromRoute] string id, RoleEditModel model)
+        {
+            var roles = await GetCompanyRoles();
+
+            if (roles.Any(t => t.Id == id))
+            {
+                var role = await _roleRepository.Read(id);
+                var permissionKeys = await PrunePermissions(model.CompanyId, model.PermissionKeys);
+                role.Id = id;
+                role.Active = model.Active;
+                role.Description = model.Description;
+                role.Name = model.Name;
+                role.CompanyId = model.CompanyId;
+                role.Permissions = string.Join(';', permissionKeys);
+                await _roleRepository.Update(role);
+            }
+
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        [PermissionAuthorize(Premissions.ManageRole)]
+        public async Task DeleteRole([FromRoute] string id)
+        {
+            var roles = await GetCompanyRoles();
+
+            if (roles.Any(t => t.Id == id))
+            {
+                await _roleRepository.Delete(id);
+            }
+
+        }
+
+        [HttpGet]
+        [Route("~/api/crosscompany/roles")]
         [PermissionAuthorize(Premissions.ViewRole, Premissions.ViewCompany)]
         public async Task<IEnumerable<RoleViewDto>> GetRoles()
         {
@@ -56,11 +143,80 @@ namespace DNVGL.Authorization.UserManagement.ApiControllers
         }
 
         [HttpGet]
-        [Route("company/{id}")]
-        [PermissionAuthorize(Premissions.ViewRole)]
-        public async Task<IEnumerable<RoleViewDto>> GetCompanyRoles([FromRoute] string id)
+        [Route("~/api/crosscompany/roles/{companyid}")]
+        [PermissionAuthorize(Premissions.ViewRole, Premissions.ViewCompany)]
+        public async Task<IEnumerable<RoleViewDto>> GetCrossCompanyRoles([FromRoute] string companyid)
         {
-            var roles = await _roleRepository.GetRolesOfCompany(id);
+            return await GetRolesByCompanyId(companyid);
+        }
+
+        [HttpGet]
+        [Route("~/api/crosscompany/roles/{id}")]
+        [PermissionAuthorize(Premissions.ViewRole, Premissions.ViewCompany)]
+        public async Task<Role> GetCrosscompanyRole([FromRoute] string id)
+        {
+            return await FetchRole(id);
+        }
+
+
+        [HttpPost]
+        [Route("~/api/crosscompany/roles")]
+        [PermissionAuthorize(Premissions.ManageRole, Premissions.ViewCompany)]
+        public async Task<string> CreateCrosscompanyRole([FromBody] RoleEditModel model)
+        {
+            var permissionKeys = await PrunePermissions(model.CompanyId, model.PermissionKeys);
+
+            var role = new Role
+            {
+                Description = model.Description,
+                Name = model.Name,
+                Active = model.Active,
+                CompanyId = model.CompanyId,
+                Permissions = string.Join(';', permissionKeys)
+            };
+            role = await _roleRepository.Create(role);
+            return role.Id;
+        }
+
+
+        [HttpPut]
+        [Route("~/api/crosscompany/roles/{id}")]
+        [PermissionAuthorize(Premissions.ManageRole, Premissions.ViewCompany)]
+        public async Task UpdateCrosscompanyRole([FromRoute] string id, RoleEditModel model)
+        {
+            var role = await _roleRepository.Read(id);
+            var permissionKeys = await PrunePermissions(model.CompanyId, model.PermissionKeys);
+            role.Id = id;
+            role.Active = model.Active;
+            role.Description = model.Description;
+            role.Name = model.Name;
+            role.CompanyId = model.CompanyId;
+            role.Permissions = string.Join(';', permissionKeys);
+            await _roleRepository.Update(role);
+        }
+
+        [HttpDelete]
+        [Route("~/api/crosscompany/roles/{id}")]
+        [PermissionAuthorize(Premissions.ManageRole, Premissions.ViewCompany)]
+        public async Task DeleteCrosscompanyRole([FromRoute] string id)
+        {
+            await _roleRepository.Delete(id);
+        }
+        private async Task<User> GetCurrentUser()
+        {
+            var varacityId = _premissionOptions.GetUserIdentity(HttpContext);
+            return await _userRepository.ReadByIdentityId(varacityId);
+        }
+
+        private async Task<IList<string>> PrunePermissions(string companyId, IList<string> sourcePermissionKeys)
+        {
+            var company = await _companyRepository.Read(companyId);
+            return sourcePermissionKeys.Where(t => company.PermissionKeys.Any(f => f == t)).ToList();
+        }
+
+        private async Task<IEnumerable<RoleViewDto>> GetRolesByCompanyId(string companyId)
+        {
+            var roles = await _roleRepository.GetRolesOfCompany(companyId);
             var allPermissions = await _permissionRepository.GetAll();
 
 
@@ -79,11 +235,7 @@ namespace DNVGL.Authorization.UserManagement.ApiControllers
             return result;
         }
 
-
-        [HttpGet]
-        [Route("{id}")]
-        [PermissionAuthorize(Premissions.ViewRole)]
-        public async Task<Role> GetRole([FromRoute] string id)
+        private async Task<Role> FetchRole(string id)
         {
             var role = await _roleRepository.Read(id);
             var allPermissions = await _permissionRepository.GetAll();
@@ -91,55 +243,6 @@ namespace DNVGL.Authorization.UserManagement.ApiControllers
             result.permissions = allPermissions.Where(p => role.PermissionKeys.Contains(p.Key));
 
             return result;
-        }
-
-        [HttpPost]
-        [Route("")]
-        [PermissionAuthorize(Premissions.ManageRole)]
-        public async Task<string> CreateRole([FromBody] RoleEditModel model)
-        {
-            var permissionKeys = await PrunePermissions(model.CompanyId, model.PermissionKeys);
-
-            var role = new Role
-            {
-                Description = model.Description,
-                Name = model.Name,
-                Active = model.Active,
-                CompanyId = model.CompanyId,
-                Permissions = string.Join(';', permissionKeys)
-            };
-            role = await _roleRepository.Create(role);
-            return role.Id;
-        }
-
-        [HttpPut]
-        [Route("{id}")]
-        [PermissionAuthorize(Premissions.ManageRole)]
-        public async Task UpdateRole([FromRoute] string id, RoleEditModel model)
-        {
-            var role = await _roleRepository.Read(id);
-            var permissionKeys = await PrunePermissions(model.CompanyId, model.PermissionKeys);
-            role.Id = id;
-            role.Active = model.Active;
-            role.Description = model.Description;
-            role.Name = model.Name;
-            role.CompanyId = model.CompanyId;
-            role.Permissions = string.Join(';', permissionKeys);
-            await _roleRepository.Update(role);
-        }
-
-        [HttpDelete]
-        [Route("{id}")]
-        [PermissionAuthorize(Premissions.ManageRole)]
-        public async Task DeleteRole([FromRoute] string id)
-        {
-            await _roleRepository.Delete(id);
-        }
-
-        private async Task<IList<string>> PrunePermissions(string companyId, IList<string> sourcePermissionKeys)
-        {
-            var company = await _companyRepository.Read(companyId);
-            return sourcePermissionKeys.Where(t => company.PermissionKeys.Any(f => f == t)).ToList();
         }
     }
 }
--- DNVGL.Authorization.UserManagement.ApiControllers/UsersController.cs
+++ DNVGL.Authorization.UserManagement.ApiControllers/UsersController.cs
@@ -16,7 +16,7 @@ namespace DNVGL.Authorization.UserManagement.ApiControllers
 {
     [Authorize]
     [ApiController]
-    [Route("api/users")]
+    [Route("api/mycompany/users")]
     public class UsersController : ControllerBase
     {
         private readonly IRole _roleRepository;
@@ -25,7 +25,7 @@ namespace DNVGL.Authorization.UserManagement.ApiControllers
         private readonly PermissionOptions _premissionOptions;
         private readonly IPermissionRepository _permissionRepository;
 
-        public UsersController(IUser userRepository, IRole roleRepository, IUserSynchronization userSynchronization,PermissionOptions premissionOptions, IPermissionRepository permissionRepository)
+        public UsersController(IUser userRepository, IRole roleRepository, IUserSynchronization userSynchronization, PermissionOptions premissionOptions, IPermissionRepository permissionRepository)
         {
             _userRepository = userRepository;
             _roleRepository = roleRepository;
@@ -34,114 +34,95 @@ namespace DNVGL.Authorization.UserManagement.ApiControllers
             _permissionRepository = permissionRepository;
         }
 
-
         [HttpGet]
         [Route("")]
-        [PermissionAuthorize(Premissions.ViewUser, Premissions.ViewCompany)]
+        [PermissionAuthorize(Premissions.ViewUser)]
         public async Task<IEnumerable<UserViewModel>> GetUsers()
         {
-            var users = await _userRepository.All();
-            var allPermissions = await _permissionRepository.GetAll();
-
-            var result = users.Select(t =>
-            {
-                var dto = t.ToViewDto<UserViewModel>();
-
-                if (t.RoleList != null)
-                {
-                    dto.Roles = t.RoleList.Select(r =>
-                    {
-                        var RoleViewDto = r.ToViewDto<RoleViewDto>();
-
-                        if (r.PermissionKeys != null)
-                        {
-                            RoleViewDto.permissions = allPermissions.Where(p => r.PermissionKeys.Contains(p.Key));
-                        }
-
-                        return RoleViewDto;
-                    });
-                }
-
-                return dto;
-            });
-
-
-            return result;
+            var user = await GetCurrentUser();
+            return await GetUsersOfCompany(user.CompanyId);
         }
-
-        [HttpGet]
-        [Route("company/{id}")]
-        [PermissionAuthorize(Premissions.ViewUser)]
-        public async Task<IEnumerable<UserViewModel>> GetCompanyUsers([FromRoute] string id)
-        {
-            var users = await _userRepository.GetUsersOfCompany(id);
-            var allPermissions = await _permissionRepository.GetAll();
-
-            var result = users.Select(t =>
-            {
-                var dto = t.ToViewDto<UserViewModel>();
-
-                if (t.RoleList != null)
-                {
-                    dto.Roles = t.RoleList.Select(r =>
-                    {
-                        var RoleViewDto = r.ToViewDto<RoleViewDto>();
-
-                        if (r.PermissionKeys != null)
-                        {
-                            RoleViewDto.permissions = allPermissions.Where(p => r.PermissionKeys.Contains(p.Key));
-                        }
-
-                        return RoleViewDto;
-                    });
-                }
-
-                return dto;
-            });
-
-
-            return result;
-        }
-
-
 
         [HttpGet]
         [Route("{id}")]
         [PermissionAuthorize(Premissions.ViewUser)]
         public async Task<UserViewModel> GetUser([FromRoute] string id)
         {
-            var allPermissions = await _permissionRepository.GetAll();
-            var user = await _userRepository.Read(id);
+            var cuurentUser = await GetCurrentUser();
+            var user = await GetUserById(id);
 
-            var result = user.ToViewDto<UserViewModel>();
+            if (cuurentUser.CompanyId == user.CompanyId)
+                return user;
 
-            if (user.RoleList != null)
-            {
-                result.Roles = user.RoleList.Select(r =>
-                {
-                    var RoleViewDto = r.ToViewDto<RoleViewDto>();
-
-                    if (r.PermissionKeys != null)
-                    {
-                        RoleViewDto.permissions = allPermissions.Where(p => r.PermissionKeys.Contains(p.Key));
-                    }
-
-                    return RoleViewDto;
-                });
-            }
-
-            result.company = user.Company.ToViewDto<CompanyViewDto>();
-
-            if (user.Company.PermissionKeys != null)
-            {
-                result.company.permissions = allPermissions.Where(p => user.Company.PermissionKeys.Contains(p.Key));
-            }
-
-            return result;
+            return null;
         }
 
+
+        [HttpPut]
+        [Route("{id}")]
+        [PermissionAuthorize(Premissions.ManageUser)]
+        public async Task UpdateUser([FromRoute] string id, UserEditModel model)
+        {
+            var cuurentUser = await GetCurrentUser();
+            var user = await _userRepository.Read(id);
+
+            if (cuurentUser.CompanyId == user.CompanyId)
+            {
+                var roleIds = await PruneRoles(cuurentUser.CompanyId, model.RoleIds);
+
+                user.Id = id;
+                user.Active = model.Active;
+                user.Description = model.Description;
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.VeracityId = model.VeracityId;
+                user.CompanyId = cuurentUser.CompanyId;
+                user.RoleIds = string.Join(';', roleIds);
+                user.Email = model.Email;
+                await _userRepository.Update(user);
+            }
+        }
+
+        [HttpPost]
+        [Route("")]
+        [PermissionAuthorize(Premissions.ManageUser)]
+        public async Task<string> CreateUser([FromBody] UserEditModel model)
+        {
+            var cuurentUser = await GetCurrentUser();
+
+            var roleIds = await PruneRoles(cuurentUser.CompanyId, model.RoleIds);
+            var user = new User
+            {
+                Description = model.Description,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                VeracityId = model.VeracityId,
+                Active = model.Active,
+                CompanyId = cuurentUser.CompanyId,
+                RoleIds = string.Join(';', roleIds),
+                Email = model.Email,
+            };
+            user = await _userRepository.Create(user);
+            return user.Id;
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        [PermissionAuthorize(Premissions.ManageUser)]
+        public async Task DeleteUser([FromRoute] string id)
+        {
+            var cuurentUser = await GetCurrentUser();
+            var user = await _userRepository.Read(id);
+
+            if (cuurentUser.CompanyId == user.CompanyId)
+            {
+                await _userRepository.Delete(id);
+            }
+        }
+
+
         [HttpGet]
-        [Route("currentUser")]
+        [Route("~/api/users/currentUser")]
         [PermissionAuthorize(Premissions.ViewUser)]
         public async Task<UserViewModel> GetUserByIdentityId()
         {
@@ -178,7 +159,7 @@ namespace DNVGL.Authorization.UserManagement.ApiControllers
 
 
         [HttpGet]
-        [Route("{id}/permissions")]
+        [Route("~/api/users/{id}/permissions")]
         [PermissionAuthorize(Premissions.ViewUser)]
         public async Task<IEnumerable<string>> GetUserPermissions([FromRoute] string id)
         {
@@ -186,10 +167,62 @@ namespace DNVGL.Authorization.UserManagement.ApiControllers
             return user.RoleList.SelectMany(t => t.PermissionKeys);
         }
 
+        [HttpGet]
+        [Route("~/api/crosscompany/users")]
+        [PermissionAuthorize(Premissions.ViewUser, Premissions.ViewCompany)]
+        public async Task<IEnumerable<UserViewModel>> GetCrossCompanyUsers()
+        {
+            var users = await _userRepository.All();
+            var allPermissions = await _permissionRepository.GetAll();
+
+            var result = users.Select(t =>
+            {
+                var dto = t.ToViewDto<UserViewModel>();
+
+                if (t.RoleList != null)
+                {
+                    dto.Roles = t.RoleList.Select(r =>
+                    {
+                        var RoleViewDto = r.ToViewDto<RoleViewDto>();
+
+                        if (r.PermissionKeys != null)
+                        {
+                            RoleViewDto.permissions = allPermissions.Where(p => r.PermissionKeys.Contains(p.Key));
+                        }
+
+                        return RoleViewDto;
+                    });
+                }
+
+                return dto;
+            });
+
+
+            return result;
+        }
+
+        [HttpGet]
+        [Route("~/api/crosscompany/{companyid}/users")]
+        [PermissionAuthorize(Premissions.ViewUser, Premissions.ViewCompany)]
+        public async Task<IEnumerable<UserViewModel>> GetCompanyUsers([FromRoute] string companyid)
+        {
+            return await GetUsersOfCompany(companyid);
+        }
+
+
+
+        [HttpGet]
+        [Route("~/api/crosscompany/users/{id}")]
+        [PermissionAuthorize(Premissions.ViewUser, Premissions.ViewCompany)]
+        public async Task<UserViewModel> GetCrossCompanyUser([FromRoute] string id)
+        {
+            return await GetUserById(id);
+        }
+
         [HttpPost]
-        [Route("")]
-        [PermissionAuthorize(Premissions.ManageUser)]
-        public async Task<string> CreateUser([FromBody] UserEditModel model)
+        [Route("~/api/crosscompany/users")]
+        [PermissionAuthorize(Premissions.ManageUser, Premissions.ViewCompany)]
+        public async Task<string> CreateCrossCompanyUser([FromBody] UserEditModel model)
         {
             var roleIds = await PruneRoles(model.CompanyId, model.RoleIds);
             var user = new User
@@ -207,10 +240,11 @@ namespace DNVGL.Authorization.UserManagement.ApiControllers
             return user.Id;
         }
 
+
         [HttpPut]
-        [Route("{id}")]
-        [PermissionAuthorize(Premissions.ManageUser)]
-        public async Task UpdateUser([FromRoute] string id, UserEditModel model)
+        [Route("~/api/crosscompany/users/{id}")]
+        [PermissionAuthorize(Premissions.ManageUser, Premissions.ViewCompany)]
+        public async Task UpdateCrossCompanyUser([FromRoute] string id, UserEditModel model)
         {
             var roleIds = await PruneRoles(model.CompanyId, model.RoleIds);
             var user = await _userRepository.Read(id);
@@ -226,29 +260,103 @@ namespace DNVGL.Authorization.UserManagement.ApiControllers
             await _userRepository.Update(user);
         }
 
-        [HttpPut]
-        [Route("sync/{id}")]
-        [PermissionAuthorize(Premissions.ManageUser)]
-        public async Task SyncUser([FromRoute] string id, UserEditModel model)
-        {
-            var user = await _userRepository.Read(id);
-
-            await _serSynchronization.SyncUser(user);
-        }
 
 
         [HttpDelete]
-        [Route("{id}")]
-        [PermissionAuthorize(Premissions.ManageUser)]
-        public async Task DeleteUser([FromRoute] string id)
+        [Route("~/api/crosscompany/users/{id}")]
+        [PermissionAuthorize(Premissions.ManageUser, Premissions.ViewCompany)]
+        public async Task DeleteCrossCompanyUser([FromRoute] string id)
         {
             await _userRepository.Delete(id);
         }
+
+
+        //[HttpPut]
+        //[Route("sync/{id}")]
+        //[PermissionAuthorize(Premissions.ManageUser)]
+        //public async Task SyncUser([FromRoute] string id, UserEditModel model)
+        //{
+        //    var user = await _userRepository.Read(id);
+
+        //    await _serSynchronization.SyncUser(user);
+        //}
 
         private async Task<IList<string>> PruneRoles(string companyId, IList<string> sourceRoleIds)
         {
             var roles = await _roleRepository.GetRolesOfCompany(companyId);
             return sourceRoleIds.Where(t => roles.Any(f => f.Id == t)).ToList();
+        }
+
+
+        private async Task<User> GetCurrentUser()
+        {
+            var varacityId = _premissionOptions.GetUserIdentity(HttpContext);
+            return await _userRepository.ReadByIdentityId(varacityId);
+        }
+
+
+        private async Task<IEnumerable<UserViewModel>> GetUsersOfCompany(string companyId)
+        {
+            var users = await _userRepository.GetUsersOfCompany(companyId);
+            var allPermissions = await _permissionRepository.GetAll();
+
+            var result = users.Select(t =>
+            {
+                var dto = t.ToViewDto<UserViewModel>();
+
+                if (t.RoleList != null)
+                {
+                    dto.Roles = t.RoleList.Select(r =>
+                    {
+                        var RoleViewDto = r.ToViewDto<RoleViewDto>();
+
+                        if (r.PermissionKeys != null)
+                        {
+                            RoleViewDto.permissions = allPermissions.Where(p => r.PermissionKeys.Contains(p.Key));
+                        }
+
+                        return RoleViewDto;
+                    });
+                }
+
+                return dto;
+            });
+
+
+            return result;
+        }
+
+
+        private async Task<UserViewModel> GetUserById(string userId)
+        {
+            var allPermissions = await _permissionRepository.GetAll();
+            var user = await _userRepository.Read(userId);
+
+            var result = user.ToViewDto<UserViewModel>();
+
+            if (user.RoleList != null)
+            {
+                result.Roles = user.RoleList.Select(r =>
+                {
+                    var RoleViewDto = r.ToViewDto<RoleViewDto>();
+
+                    if (r.PermissionKeys != null)
+                    {
+                        RoleViewDto.permissions = allPermissions.Where(p => r.PermissionKeys.Contains(p.Key));
+                    }
+
+                    return RoleViewDto;
+                });
+            }
+
+            result.company = user.Company.ToViewDto<CompanyViewDto>();
+
+            if (user.Company.PermissionKeys != null)
+            {
+                result.company.permissions = allPermissions.Where(p => user.Company.PermissionKeys.Contains(p.Key));
+            }
+
+            return result;
         }
     }
 }
