e9c9a0f150b0eee19a67f09caa597cb999a45e7f
Steven Mark Alford <Steven.Mark.Alford@dnv.com>
2021-09-03 11:29:46 +0800

    Remove hardcoded dev ops configuration.

--- DNV.SecretsManager.ConsoleApp/Program.cs
+++ DNV.SecretsManager.ConsoleApp/Program.cs
@@ -8,15 +8,7 @@ namespace DNV.SecretsManager.ConsoleApp
 {
 	class Program
 	{
-		private const string AzureSubscriptionId = "d288cb4f-5356-481f-a571-11005977e590";
-
-		private static VariableGroupClientConfiguration _variableGroupClientConfiguration = new VariableGroupClientConfiguration
-		{
-			BaseUrl = "https://dnvgl-one.visualstudio.com",
-			Organization = "Veracity",
-			ApiVersion = "6.1-preview.2",
-			PersonalAccessToken = "4gepm6cenmvsc3cox2hzymkddvof4dkx5xpwgi6x34tfcylyl6pa"
-		};
+		private static VariableGroupClientConfiguration _variableGroupClientConfiguration;
 
 		static async Task Main(string[] args)
 		{
@@ -60,6 +52,22 @@ namespace DNV.SecretsManager.ConsoleApp
 			}
 			else if (sourceChoice.Equals("v"))
 			{
+				Console.WriteLine("Please enter the base URL for Azure DevOps instance:");
+				var baseUrl = Console.ReadLine();
+
+				Console.WriteLine("Please enter Organization or Poject name:");
+				var organization = Console.ReadLine();
+
+				Console.WriteLine("Please enter Personal Access Token (PAT):");
+				var pat = Console.ReadLine();
+
+				_variableGroupClientConfiguration = new VariableGroupClientConfiguration
+				{
+					BaseUrl = baseUrl,
+					Organization = organization,
+					PersonalAccessToken = pat
+				};
+
 				Console.WriteLine("Please enter the id of the Variable Group in Azure DevOps:");
 				var variableGroupId = Console.ReadLine();
 
@@ -99,24 +107,24 @@ namespace DNV.SecretsManager.ConsoleApp
 		{
 			var stopwatch = Stopwatch.StartNew();
 			var secretsService = new KeyVaultSecretsService();
-            try
-            {
-                var secrets = await secretsService.GetSecretsAsDictionary(keyVaultBaseUrl);
-                var result = secretsService.ToJson(secrets);
-                await File.WriteAllTextAsync(targetFilename, result, System.Text.Encoding.UTF8);
-                stopwatch.Stop();
-                return new CommandResult
-                {
-                    Count = secrets.Count,
-                    ElapsedTime = stopwatch.Elapsed
-                };
+			try
+			{
+				var secrets = await secretsService.GetSecretsAsDictionary(keyVaultBaseUrl);
+				var result = secretsService.ToJson(secrets);
+				await File.WriteAllTextAsync(targetFilename, result, System.Text.Encoding.UTF8);
+				stopwatch.Stop();
+				return new CommandResult
+				{
+					Count = secrets.Count,
+					ElapsedTime = stopwatch.Elapsed
+				};
 			}
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-        }
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				throw;
+			}
+		}
 
 		private static async Task<CommandResult> DownloadVariableGroup(string variableGroupId, string targetFilename)
 		{
@@ -139,22 +147,22 @@ namespace DNV.SecretsManager.ConsoleApp
 			var content = await File.ReadAllTextAsync(sourceFilename);
 			var secretsService = new KeyVaultSecretsService();
 			var secrets = secretsService.FromJson(content);
-            try
-            {
-                await secretsService.SetSecretsFromJson(keyVaultBaseUrl, content);
-                stopwatch.Stop();
-                return new CommandResult
-                {
-                    Count = secrets.Count,
-                    ElapsedTime = stopwatch.Elapsed
-                };
+			try
+			{
+				await secretsService.SetSecretsFromJson(keyVaultBaseUrl, content);
+				stopwatch.Stop();
+				return new CommandResult
+				{
+					Count = secrets.Count,
+					ElapsedTime = stopwatch.Elapsed
+				};
 			}
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-        }
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				throw;
+			}
+		}
 
 		private static async Task<CommandResult> UploadVariableGroup(string sourceFilename, string variableGroupId)
 		{
