bf93af698d6b8a8806d1c4394504db8db7f8413d
Yang, Min Albert <Albert.Yang@dnv.com>
2022-03-16 20:30:44 +0800

    refactoring interface IQuerableRepository

--- DDD/DNV.Application.Abstractions/IQueryableRepository.cs
+++ DDD/DNV.Application.Abstractions/IQueryableRepository.cs
@@ -9,14 +9,13 @@ using DNVGL.Domain.Seedwork;
 
 namespace DNV.Application.Abstractions
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public interface IQueryableRepository<T>: IRepository<T> where T : Entity, IAggregateRoot
-    {
-        Task<IEnumerable<T>> QueryAsync(Expression<Func<T, bool>> predict, SortParam sortParam, CancellationToken cancellationToken = default);
-
-        Task<PaginatedResult<T>> QueryAsync(Expression<Func<T, bool>> predict, PageParam pageParam, SortParam? sortParam = null, CancellationToken cancellationToken = default);
-    }
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IQueryableRepository<T> where T : Entity
+	{
+		Task<PaginatedResult<T>> QueryAsync(Expression<Func<T, bool>> predict, PageParam pageParam,
+			SortParam? sortParam = null, CancellationToken cancellationToken = default);
+	}
 }
