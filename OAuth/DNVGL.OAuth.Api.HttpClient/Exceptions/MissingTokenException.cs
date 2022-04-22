cd1e281685a444586b88204a62e9168180d2265e
Alford <Steven.Mark.Alford@dnv.com>
2022-04-22 10:44:36 +0800

    Introduce missing token exception.

---
+++ OAuth/DNVGL.OAuth.Api.HttpClient/Exceptions/MissingTokenException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DNVGL.OAuth.Api.HttpClient.Exceptions
+{
+	[Serializable]
+	public class MissingTokenException : Exception
+	{
+		public MissingTokenException() : base("Authentication token was null or empty.") { }
+
+		private MissingTokenException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+	}
+}
